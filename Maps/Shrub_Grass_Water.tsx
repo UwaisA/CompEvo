1bb2bbf737dfc7b39ef89c95ebab0f4bf22a4677
chugorules <charlieugo@Charlies-MacBook-Pro.local>
2015-12-07 23:05:56 +0000

    Map tmx, tsx and png fix

--- Maps/Shrub_Grass_Water.tsx
+++ Maps/Shrub_Grass_Water.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Shrub_Grass_Water" tilewidth="64" tileheight="64" tilecount="40">
  <tileoffset x="0" y="15"/>
- <image source="../../../Documents/Imperial/Year 3/Project/Shrub Grass Water.png" width="512" height="320"/>
+ <image source="../../../GitHub/CompEvo/Maps/Shrub Grass Water.png" width="512" height="320"/>
  <terraintypes>
   <terrain name="Grass" tile="-1"/>
   <terrain name="Water" tile="-1"/>
